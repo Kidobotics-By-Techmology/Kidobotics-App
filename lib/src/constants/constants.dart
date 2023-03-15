123ebd46d8068c9b62ca29507e3c2e1d62552e03
Sunny✨ <Sundaypius2000@gmail.com>
2023-03-15 23:29:39 +0100

    Update constant file

---
+++ lib/src/constants/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+TextStyle kTextStyle = const TextStyle(
+  color: Colors.black,
+  fontSize: 16,
+  fontWeight: FontWeight.w500,
+);
